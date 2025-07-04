1aae8ef21018988431eea8b1430250a60789eb9e
RenanCruz7 <renancruz199@gmail.com>
2025-07-04 18:41:47 -0300

    Reset CSS e teste de componente e inicializacao do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Toolbar, Typography } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Toolbar sx={{ 
+        bgcolor: '#8A2BE2', // Cor roxa (BlueViolet)
+        color: 'white',
+        borderRadius: 1,
+        boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)'
+      }}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Teste de Toolbar
+        </Typography>
+      </Toolbar>
     </>
   )
 }
