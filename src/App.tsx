b329976173a7454633b03d1fbc3c46a356a3236f
RenanCruz7 <renancruz199@gmail.com>
2025-07-07 17:00:33 -0300

    Feat: Colocando Avatar, Setting e Notification na Topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import './App.css'
-import { Toolbar, Typography } from '@mui/material'
+import "./App.css";
+import { Avatar, Badge, Toolbar, Typography } from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import maxImage from "./assets/max.png";
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function App() {
   return (
     <>
-      <Toolbar sx={{ 
-        bgcolor: '#8A2BE2', // Cor roxa (BlueViolet)
-        color: 'white',
-        borderRadius: 1,
-        boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)'
-      }}>
+      <Toolbar
+        sx={{
+          bgcolor: "#8A2BE2", // Cor roxa (BlueViolet)
+          color: "white",
+          borderRadius: 1,
+          boxShadow:
+            "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
+        }}
+      >
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Teste de Toolbar
+          F1 Dashboard 2025
         </Typography>
+        <Avatar sx={{ width: 40, height: 40, marginRight: 2 }} src={maxImage}></Avatar>
+        <Badge badgeContent={4} color="primary">
+          <MailIcon color="inherit" />
+        </Badge>
+        <SettingsIcon sx={{ color: "white", marginLeft: 2 }} />
       </Toolbar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
